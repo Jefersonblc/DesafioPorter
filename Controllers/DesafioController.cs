3e2f60b69b5040246dccad9c2c19dc920d36e20d
Jeferson Correa <Jefersonblc@gmail.com>
2023-07-12 15:17:09 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/DesafioController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesafioPorter.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class DesafioController : ControllerBase
+    {
+        [HttpGet(Name = "NumberToWords")]
+        public ActionResult<string> NumberToWords(int number)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet(Name = "ArraySum")]
+        public ActionResult<string> ArraySum(params int[] numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet(Name = "EvaluateExpression")]
+        public ActionResult<int> EvaluateExpression(string expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet(Name = "UniqueList")]
+        public ActionResult<string> UniqueList(params object[] objects)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
