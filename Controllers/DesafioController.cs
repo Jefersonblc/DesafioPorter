33c0f2b42e8a8521619ec49cf09b8db9dbe58d35
Jeferson Correa <Jefersonblc@gmail.com>
2023-07-12 20:03:45 -0300

    Sum integers in array

--- Controllers/DesafioController.cs
+++ Controllers/DesafioController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DesafioPorter.Helpers;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
 
 namespace DesafioPorter.Controllers
 {
@@ -7,26 +12,47 @@ namespace DesafioPorter.Controllers
     [ApiController]
     public class DesafioController : ControllerBase
     {
+
+        /* Implemente uma função que recebe um número inteiro e retorne uma string
+         * com a representação por extenso desse número.
+         * Exemplo: 123 -> "cento e vinte e três".
+         */
         [HttpGet(Name = "NumberToWords")]
         public ActionResult<string> NumberToWords(int number)
         {
             throw new NotImplementedException();
         }
 
-        [HttpGet(Name = "ArraySum")]
-        public ActionResult<string> ArraySum(params int[] numbers)
+        /* Implemente uma função que recebe um array de inteiros e retorne a soma
+         * desses números. O array pode ter até 1 milhão de números.
+         */
+        [HttpPost(Name = "ArraySum")]
+        public ActionResult<string> ArraySum(int[] numbers)
         {
-            throw new NotImplementedException();
+            // Easy way, not optimal
+            // var sum = numbers.Sum();
+
+            // My way
+            var sum = new Calculator().SumArray(numbers);
+
+            return sum.ToString();
         }
 
+        /* Implemente uma função que recebe uma string contendo uma expressão
+         * matemática simples (sem parênteses) e retorne o resultado dessa expressão.
+         * Exemplo: "2 + 3 * 5" -> 17.
+         */
         [HttpGet(Name = "EvaluateExpression")]
         public ActionResult<int> EvaluateExpression(string expression)
         {
             throw new NotImplementedException();
         }
 
+        /* Implemente uma função que recebe uma lista de objetos e retorne uma
+         * nova lista apenas com os objetos únicos, ou seja, sem repetições.
+         */
         [HttpGet(Name = "UniqueList")]
-        public ActionResult<string> UniqueList(params object[] objects)
+        public ActionResult<string> UniqueList(object[] objects)
         {
             throw new NotImplementedException();
         }
---
+++ Helpers/Calculator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Numerics;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace DesafioPorter.Helpers
+{
+    public class Calculator
+    {
+        public long SumArray(int[] numbers)
+        {
+            long sum = 0;
+
+            //Two easy ways that work better, but not optimal yet
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    sum += numbers[i];
+            //}
+
+            //foreach (int number in numbers)
+            //{
+            //    sum += number;
+            //}
+
+            //Trying to optimize:
+
+            //var isOdd = numbers.Length % 2 != 0;
+
+            //for (int i = 0; i < numbers.Length; i += 2)
+            //{
+            //    sum += isOdd && i + 1 >= numbers.Length 
+            //        ? numbers[i] 
+            //        : numbers[i] + numbers[i + 1];
+            //}
+
+            //Optimised, at least 2 times faster for big arrays:
+
+            if(numbers != null && numbers.Length > 0) 
+            { 
+                int index = 0;
+                for (int i = 0; i < numbers.Length; index = i += 5)
+                {
+                    if (index + 4 >= numbers.Length)
+                    {
+                        break;
+                    }
+                    sum += numbers[i] + numbers[i + 1] + numbers[i + 2] + numbers[i + 3] + numbers[i + 4];
+                }
+
+                for (int i = index; i < numbers.Length; i++)
+                {
+                    sum += numbers[i];
+                }
+            } 
+
+            return sum;
+        }
+
+    }
+}
